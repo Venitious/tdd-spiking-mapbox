b5f87a98b6fe4cc610e7fdb7054efa15d0b4cb0e
venitious <george_olid@hotmail.com>
2023-06-08 11:12:33 +0100

    add customisable icons

--- my-app/src/Components/Map.jsx
+++ my-app/src/Components/Map.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import { Icon } from "leaflet";
  
 export const Map = () => {
-  const position = [51.4934, 0.0098]; // [latitude, longitude]
+  const position2 = [51.4934, 0.0098]; // [latitude, longitude]
   const zoomLevel = 13;
+
+  const position = [51.50805832397229, -0.1280274575807878]
+  const eggPosition = new Icon ({
+    iconUrl: "/easter.png",
+    iconSize: [30, 125],
+    iconAnchor: [40, 90],
+    popupAnchor: [-25, -40]
+  })
  
+
   
 
   return (
@@ -17,6 +27,16 @@ export const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      <Marker position={position} icon={eggPosition}>
+        <Popup>
+          "Beautiful town centre"
+        </Popup>
+      </Marker>
+      <Marker position={position2}>
+        <Popup>
+        "Make a wish, but do not take a drink"
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
